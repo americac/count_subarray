e9cb5e9ae0041985322c1fec830a5440c831675d
America Calleros <america.quintero@gmail.com>
2024-04-08 16:02:16 -0500

    Break out main method into smaller methods

--- count_subarray.rb
+++ count_subarray.rb
@@ -1,26 +1,35 @@
 def count_subarrays(number_array, k)
-  results = 0
-  max_number = number_array.max
+  @results = 0
+  @k = k
+  @number_array = number_array
 
   while number_array.length.positive?
-    max_was_found = 0
-    number_array.each do |number|
-      next unless number == max_number
-
-      max_was_found += 1
-      results += 1 if max_was_found >= k
-    end
-
+    find_subarrays(number_array)
     number_array.shift
   end
 
-  results
+  @results
+end
+
+def find_subarrays(array_of_ints)
+  max_was_found = 0
+
+  array_of_ints.each do |number|
+    next unless number == max_number
+
+    max_was_found += 1
+    @results += 1 if max_was_found >= @k
+  end
+end
+
+def max_number
+  @max_number ||= @number_array.max
 end
 
 result = count_subarrays([1, 3, 2, 3, 3], 2)
-message = result == 6 ? 'Sucess' : 'Failed'
+message = result == 6 ? 'Passed' : 'Failed'
 puts message
 
 result = count_subarrays([1, 4, 2, 1], 3)
-message = result == 0 ? 'Sucess' : 'Failed'
+message = result.zero? ? 'Passed' : 'Failed'
 puts message
